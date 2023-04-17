522b779076b7cf81fef7017984baba1008d65451
Djordje Mirosavljevic <djordje.mirosavljevic@dci.education>
2023-04-17 13:43:08 +0200

    first

---
+++ src/server.js
@@ -0,0 +1,21 @@
+import fs from "fs";
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.set(cors());
+const port = 3322;
+
+app.get("/", (req, res) => {
+  res.send("mernbasic API");
+});
+
+app.get("/employees", (req, res) => {
+  const rawEmployees = fs.readFileSync("./src/data/employees.json", "utf8");
+  const employees = JSON.parse(rawEmployees);
+  res.send(employees);
+});
+
+app.listen(port, () => {
+  console.log(`listening at http://localhost:${port}`);
+});
